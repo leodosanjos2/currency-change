7384e4286dc66ff4c814f0c0cd2fe58fd73dbf2c
leodosanjos2 <leodosanjoss2@hotmail.com>
2025-06-18 17:47:27 -0300

    Rechanged the InputBox component to type number by making the conditions directly when call the method and simplifying the useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,51 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from 'react'
 import './App.css'
 import useCurrencyInfo from './hooks/useCurrencyHooks';
 import { InputBox } from './components/index.js';
 
 function App() {
-  const [amount, setAmount] = useState(0);
-  const [from, setFrom] = useState('usd');
-  const [to, setTo] = useState('brl');
-  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState('usd')
+  const [to, setTo] = useState('brl')
+  const [convertedAmount, setConvertedAmount] = useState(0)
 
-  const currencyInfo = useCurrencyInfo(from);
-  const options = Object.keys(currencyInfo);
+  const currencyInfo = useCurrencyInfo(from)
+  const options = Object.keys(currencyInfo)
+
+  const convert = () => {
+    if(amount > 0 && currencyInfo[to]){
+    setConvertedAmount(amount * currencyInfo[to])
+    }
+  }
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+  }
 
   useEffect(() => {
-    if (amount>0){
-      if (currencyInfo && currencyInfo[to]) {
-        setConvertedAmount(amount * currencyInfo[to]);
-      }
-    }else{
-      setConvertedAmount(0);
-    }
-    },[amount, from, to, currencyInfo]);
+    convert();
+  }, [from, to, amount, currencyInfo]);
 
-    const swap = ()=>{
-      setFrom(to);
-      setTo(from);
-      
-    }
-
-    return (
-    <div className='w-full h-screen flex flex-wrap
-    justify-center items-center bg-cover bg-no-repeat'
-    style={{backgroundImage: 
-    `url(https://images.pexels.com/photos/4386442/pexels-photo-4386442.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`}}>
+  return (
+    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
+    style={{backgroundImage: `url(https://images.pexels.com/photos/4386442/pexels-photo-4386442.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`}}
+    >
       <div className='w-full'>
-        <div className='w-full max-w-md mx-auto border 
-        border-gray-60 rounded-lg p-5 backdrop-blur-sm
-        bg-white/30'>
-          <form onSubmit={(e)=>e.preventDefault()}>
-            <div className="flex justify-center mb-4">
-            <label className='bg-blue-600 text-white px-4 py-3 rounded-lg text-center w-full max-w-md'>
-            Convert {from.toUpperCase()} to {to.toUpperCase()}
-            </label>
-            </div>
+        <div className='w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30'>
+          <form onSubmit={(e) => {
+            e.preventDefault()
+            convert()
+          }}>
+            <div className="w-full flex justify-center">
+              <div className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg text-center">
+                Convert {from.toUpperCase()} to {to.toUpperCase()}
+              </div>
+              </div>
             <div className='w-full mb-1'>
               <InputBox
               label="from"
-              amount={amount}
+              amount={amount === 0? '': amount}
               currencyOptions={options}
               onCurrencyChange={(currency) => setFrom(currency)}
               onAmountChange={(amount) => setAmount(amount)}
@@ -54,26 +53,25 @@ function App() {
               />
             </div>
             <div className='relative w-full h-0.5'>
-              <button className='absolute left-1/2 -translate-x-1/2 
-              -translate-y-1/2 border-2 border-white rounded-md
-              bg-blue-600 text-white px-2 py-0.5'
+              <button
+              className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5'
               onClick={swap}
               >Swap</button>
             </div>
             <div className='w-full mb-1'>
               <InputBox
               label="to"
-              amount={convertedAmount}
               currencyOptions={options}
+              amount={convertedAmount === 0 || amount === 0 ? '': convertedAmount}
               onCurrencyChange={(currency) => setTo(currency)}
               selectedCurrency={to}
               amountDisabled
-              onKey
               />
             </div>
           </form>
         </div>
       </div>
+
     </div>
   )
 }
